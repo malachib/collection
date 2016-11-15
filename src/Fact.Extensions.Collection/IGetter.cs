2fdcaa2285788d60979598b929980ab04e0524ed
Malachi Burke <malachi.burke@gmail.com>
2016-11-15 13:33:46 -0800

    first commit

---
+++ src/Fact.Extensions.Collection/IGetter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fact.Apprentice.Collection
+{
+    public interface IGetter<TKey, TValue>
+    {
+        TValue Get(TKey key, Type type);
+    }
+
+    public interface IGetterAsync<TKey, TValue>
+    {
+        Task<TValue> GetAsync(TKey key, Type type);
+    }
+}
