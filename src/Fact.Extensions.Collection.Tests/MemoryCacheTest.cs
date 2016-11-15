86519c276facfca7eb7eedacf96e58d2e1ab5df0
Malachi Burke <malachi.burke@gmail.com>
2016-11-15 13:33:46 -0800

    Adding boilerplate for unit tests

---
+++ src/Fact.Extensions.Collection.Tests/MemoryCacheTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fact.Extensions.Collection.Tests
+{
+    [TestClass]
+    public class MemoryCacheTest
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+
+        }
+    }
+}
