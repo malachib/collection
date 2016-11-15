9d70e7fa102b93952638f62bf1770355fbea6580
Malachi Burke <malachi.burke@gmail.com>
2016-11-15 13:33:46 -0800

    More untested prep for NuGet deploy

--- src/upload_project.sh
+++ src/upload_project.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+pushd $1
+
+ASSEMBLY_NAME=$1.$VERSION
+ASSEMBLY_PATH=bin/Debug/$ASSEMBLY_NAME
+
 dotnet pack --version-suffix=$VERSION_SUFFIX
 
-$NUGET push $1.$VERSION.nupkg $NUGET_KEY -Source $NUGET_SOURCE
-$NUGET push $1.$VERSION.symbols.nupkg $NUGET_KEY -Source $NUGET_SOURCE
+$NUGET push $ASSEMBLY_PATH.nupkg $NUGET_KEY -Source $NUGET_SOURCE
+$NUGET push $ASSEMBLY_PATH.nupkg $NUGET_KEY -Source $NUGET_SOURCE
+
+popd
