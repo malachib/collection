d9d5f8482e5b4c14a676d700e128045905e1a5fa
Malachi Burke <malachi.burke@gmail.com>
2016-11-15 13:33:46 -0800

    Prepping Serialization nuget targets

--- src/upload_all.sh
+++ src/upload_all.sh
@@ -4,5 +4,7 @@
 
 ./upload_project.sh Fact.Extensions.Collection
 ./upload_project.sh Fact.Extensions.Collection.Cache
+./upload_project.sh Fact.Extensions.Serialization
+./upload_project.sh Fact.Extensions.Serialization.Newtonsoft
 
 
