aafba57e6f2bd63df80b8662a3ad1f8677bdc00f
Malachi Burke <malachi.burke@gmail.com>
2016-11-15 13:33:46 -0800

    Adding raw IAccessor and IIndexer directly from Apprentice

---
+++ src/Fact.Extensions.Collection/IAccessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Fact.Extensions.Collection
+{
+    /// <summary>
+    /// Accessor like IIndexer, but for read-only operations
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    /// <typeparam name="TKey"></typeparam>
+    public interface IAccessor<TKey, TValue>
+    {
+        TValue this[TKey key] { get; }
+    }
+
+    /// <summary>
+    /// Reusable template for standard read-only indexer operations whose lookup key is a string.
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    /// <remarks>
+    /// Be mindful to mainly use this on implementing classes, not
+    /// so much on consumers due to the way IIndexer and IAccessor mesh 
+    /// together
+    /// </remarks>
+    public interface INamedAccessor<TValue> : IAccessor<string, TValue> { }
+}
---
+++ src/Fact.Extensions.Collection/IIndexer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Fact.Extensions.Collection
+{
+    /// <summary>
+    /// Indexer reusable base interface
+    /// Hides base IAccessor - so if implementing explicitly, will need two "gets"
+    /// since .NET can't mesh the get from one interface and the set from the other
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    /// <typeparam name="TKey"></typeparam>
+    public interface IIndexer<TKey, TValue> : IAccessor<TKey, TValue>
+    {
+        new TValue this[TKey key] { get; set; }
+    }
+
+    /// <summary>
+    /// Reusable template for stock standard indexer operations
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    public interface INamedIndexer<TValue> : INamedAccessor<TValue>, IIndexer<string, TValue>
+    {
+    }
+}
