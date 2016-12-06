2461be97b62a29d118eaf883c2fe5ff85c7a1167
Malachi Burke <malachi.burke@gmail.com>
2016-12-05 16:56:10 -0800

    Adding IBag interception code

---
+++ src/Fact.Extensions.Collection.Interceptor/BagInterceptor.cs
@@ -0,0 +1,39 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Fact.Extensions.Collection.Interceptor
+{
+    public class BagInterceptor : NamedPropertyInterceptor
+    {
+        readonly IBag bag;
+
+        // FIX: Needs cleanup, definitely redundant and sloppy
+        protected override string ResolveName(System.Reflection.PropertyInfo prop)
+        {
+            if (resolveName != null)
+                return resolveName(prop);
+
+            return nameResolver != null ? nameResolver.ResolveName(prop) : prop.Name;
+        }
+
+        public BagInterceptor(IBag bag, ResolveNameDelegate resolveName = null)
+            : base(null, null, resolveName)
+        {
+            this.bag = bag;
+        }
+
+        protected override object Get(IInvocation invocation, PropertyInfo prop)
+        {
+            return bag.Get(ResolveName(prop), prop.PropertyType);
+        }
+
+        protected override void Set(IInvocation invocation, PropertyInfo prop, object value)
+        {
+            bag.Set(ResolveName(prop), value, prop.PropertyType);
+        }
+    }
+}
---
+++ src/Fact.Extensions.Collection.Interceptor/IBag.Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fact.Extensions.Collection.Interceptor
+{
+    public static class IBag_Extensions
+    {
+        // FIX: temporarily in use until we move this into actual interceptor lib
+        public static T ToInterface<T>(this IBag bag)
+            where T : class
+        {
+            var interceptor = new BagInterceptor(bag);
+            return AssemblyGlobal.Proxy.CreateInterfaceProxyWithoutTarget<T>(interceptor);
+        }
+    }
+}
