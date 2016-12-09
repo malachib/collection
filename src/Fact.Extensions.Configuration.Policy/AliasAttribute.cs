713c7cdefe0c59a818060b5cd89c4e16610a3e9c
Malachi Burke <malachi.burke@gmail.com>
2016-12-09 10:43:22 -0800

    Bringing in Policy code from Apprentice
    Untested and needs cleaning

---
+++ src/Fact.Extensions.Configuration.Policy/AliasAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fact.Extensions.Configuration
+{
+    /// <summary>
+    /// For use in 2 major scenarios:
+    /// 
+    /// a) when an OR entity strongly-typed name is one thing but the underlying sql column name is another, use alias to denote actual SQL column name
+    /// b) Maps a configuration element's property to a slightly differently named underlying app.config setting
+    /// </summary>
+    public class AliasAttribute : Attribute//, INamed
+    {
+        public AliasAttribute(string name) { Name = name; }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
