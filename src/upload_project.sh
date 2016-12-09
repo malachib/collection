eae449806b5f72b6663a8931810592cc83da8249
Malachi Burke <malachi.burke@gmail.com>
2016-12-09 10:43:22 -0800

    Fixing incorrect symbolic debug upload code

--- src/upload_project.sh
+++ src/upload_project.sh
@@ -8,6 +8,6 @@ ASSEMBLY_PATH=bin/Debug/$ASSEMBLY_NAME
 dotnet pack --version-suffix=$VERSION_SUFFIX
 
 $NUGET push $ASSEMBLY_PATH.nupkg $NUGET_KEY -Source $NUGET_SOURCE
-$NUGET push $ASSEMBLY_PATH.nupkg $NUGET_KEY -Source $NUGET_SOURCE
+$NUGET push $ASSEMBLY_PATH.symbols.nupkg $NUGET_KEY -Source $NUGET_SOURCE
 
 popd
