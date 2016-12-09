f37a6282b63dc1115ae78a1ab823f5b2f892b85e
Malachi Burke <malachi.burke@gmail.com>
2016-12-09 09:57:28 -0800

    Version bump

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export VERSION_PREFIX=1.0.0
-export VERSION_SUFFIX=alpha012
+export VERSION_SUFFIX=alpha013
 export VERSION=$VERSION_PREFIX-$VERSION_SUFFIX
 export NUGET_KEY=73f3dd5e-d8f0-4acd-aa3a-889843cb27fe
 export NUGET_SOURCE=https://www.myget.org/F/malachib/api/v2/package
