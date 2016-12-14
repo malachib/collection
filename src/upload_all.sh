56ec80bf44a7eaa91314e95bf5662ddafb069ffd
Malachi Burke <malachi.burke@gmail.com>
2016-12-14 11:42:18 -0800

    Ensuring Policy & Pipeline experimental assemblies are uploaded

--- src/upload_all.sh
+++ src/upload_all.sh
@@ -6,8 +6,8 @@
 ./upload_project.sh Fact.Extensions.Collection
 ./upload_project.sh Fact.Extensions.Collection.Interceptor
 ./upload_project.sh Fact.Extensions.Configuration
+./upload_project.sh Fact.Extensions.Configuration.Policy
 ./upload_project.sh Fact.Extensions.Serialization
 ./upload_project.sh Fact.Extensions.Serialization.Newtonsoft
 ./upload_project.sh Fact.Extensions.Serialization.MessagePack
-
-
+./upload_project.sh Fact.Extensions.Serialization.Pipelines
