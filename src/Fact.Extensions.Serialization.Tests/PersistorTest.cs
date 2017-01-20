202b0d82b774fcb8d57ddbd8dddb57b2946fe4ac
Malachi Burke <malachi.burke@gmail.com>
2017-01-19 19:15:19 -0800

    "method3" unit test working OK

--- src/Fact.Extensions.Serialization.Tests/PersistorTest.cs
+++ src/Fact.Extensions.Serialization.Tests/PersistorTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Fact.Extensions.Collection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -14,12 +15,40 @@ namespace Fact.Extensions.Serialization.Tests
         class TestRecord2
         {
             internal const string FIELD1_INITIAL_VALUE = "I am field 1";
+            internal const string FIELD2_INITIAL_VALUE = "I am field 2";
+
             [Persist]
             internal string field1 = FIELD1_INITIAL_VALUE;
 
-            internal string field2 = "I am field 2";
+            internal string field2 = FIELD2_INITIAL_VALUE;
 
             internal int field3 = 3;
+        }
+
+
+        class TestRecord2Persistor : Persistor
+        {
+            //readonly IPropertySerializer propertySerializer;
+            //readonly IPropertyDeserializer propertyDeserializer;
+            Dictionary<string, object> dictionary = new Dictionary<string, object>();
+
+            public void Persist(ref string field2, ref int field3)
+            {
+                if(Mode == ModeEnum.Serialize)
+                {
+                    dictionary["field2_renamed"] = field2;
+                    //propertySerializer.Set("field2_renamed", field2);
+                }
+                else
+                {
+                    // TODO:
+                    // beware that field3 not getting assigned here essentially zeroes it
+                    // add an on-by-default feature which reads out field values and stuffs them
+                    // into propertyValues so that the defaults match what's in the instance already
+                    field2 = (string) dictionary["field2_renamed"];
+                    //field2 = propertyDeserializer.Get<string>("field2_renamed");
+                }
+            }
         }
 
         [TestMethod]
@@ -50,6 +79,23 @@ namespace Fact.Extensions.Serialization.Tests
             p.Persist(testRecord);
 
             Assert.AreEqual(TestRecord2.FIELD1_INITIAL_VALUE, testRecord.field1);
+        }
+
+
+        [TestMethod]
+        public void JsonPersistor2Test()
+        {
+            var testRecord = new TestRecord2();
+            var p = new JsonPersistor(null, null, new TestRecord2Persistor());
+
+            p.Mode = Persistor.ModeEnum.Serialize;
+            p.Persist(testRecord);
+
+            p.Mode = Persistor.ModeEnum.Deserialize;
+
+            testRecord.field2 = "Got blasted over";
+            p.Persist(testRecord);
+            Assert.AreEqual(TestRecord2.FIELD2_INITIAL_VALUE, testRecord.field1);
         }
     }
 }
