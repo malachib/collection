e8d196b525c93653a41a54368c9987716700e0a4
Malachi Burke <malachi.burke@gmail.com>
2017-01-26 21:01:30 -0800

    Adding boilerplate unit test for Policy code

---
+++ src/Fact.Extensions.Configuration.Tests/PolicyTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fact.Extensions.Configuration.Tests
+{
+    [TestClass]
+    public class PolicyTests
+    {
+        [Alias("fact.policy.synthetic")]
+        public class SyntheticPolicy : IPolicy
+        {
+
+        }
+
+        [TestMethod]
+        public void Policy1Test()
+        {
+            var pp = new PolicyProvider();
+
+            var p = pp.GetPolicy<SyntheticPolicy>();
+        }
+    }
+}
