f05a289208f4db91c5d6e13af99d8d6208bb148d
Malachi Burke <malachi.burke@gmail.com>
2017-02-06 20:13:37 -0800

    Adding bump & more configurable project version
    * bump increments a branch-specific version number

---
+++ src/bump.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# bumps a version # up for this branch
+# TODO: add a provision for reset
+# TODO: consolidate branch_name discovery into useful-script utility
+
+branch_name=$(git symbolic-ref -q HEAD)
+branch_name=${branch_name##refs/heads/}
+branch_name=${branch_name:-HEAD}
+
+filename=../.version.$branch_name 
+
+branch_version=$(<$filename)
+
+if [ -z "$branch_version" ]
+then
+	echo "Unable to bump branch version.  Ensure file '$filename' exists"
+else
+	((branch_version++))
+
+	echo $branch_version > $filename
+fi
--- src/setenv.sh
+++ src/setenv.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# TODO: move this out into "useful-scripts" project
+
 . ../ext/myget/setenv_nuget.sh
 
 branch_name=$(git symbolic-ref -q HEAD)
@@ -8,19 +10,27 @@ branch_name=${branch_name:-HEAD}
 
 branch_version=$(<../.version.$branch_name)
 
+project_version=$(<../.project-version)
+
 if [ -z "$branch_version" ]
 then
 	echo "No version available for this branch.  Do not upload NUGET packages"
 	branch_version="0"
 fi
 
+if [ -z $project_version ]
+then
+	echo "No version available for this project.  Do not upload NUGET packages"
+	project_version="0.0.1"
+fi
+
 printf -v branch_version "%03d" $branch_version
 
-export VERSION_PREFIX=1.0.0
+export VERSION_PREFIX=$project_version
 export VERSION_SUFFIX=$branch_name$branch_version
 export VERSION=$VERSION_PREFIX-$VERSION_SUFFIX
 
-echo "Using VERSION_SUFFIX=$branch_name$branch_version"
+echo "Using VERSION=$VERSION"
 
-export branch_name
-export branch_version
+#export branch_name
+#export branch_version
