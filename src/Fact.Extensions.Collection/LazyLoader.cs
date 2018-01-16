3883ff0dae8de5f1995b036a94e42a910cfb114e
Malachi Burke <malachi.burke@gmail.com>
2018-01-16 10:55:23 -0800

    Adding more comments + System.Lazy helper methods

--- src/Fact.Extensions.Collection/LazyLoader.cs
+++ src/Fact.Extensions.Collection/LazyLoader.cs
@@ -6,12 +6,22 @@ namespace Fact.Extensions.Memory
 {
     /// <summary>
     /// Follows the lazy-load paradigm, won't allocate until value is actually requested
+    /// Differs slightly from System.Lazy in that this is a struct, and consequently can
+    /// be more easily used alongside other structs.  
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    /// <remarks>NOTE: Might duplicate System.Lazy functionality, look into this</remarks>
+    /// <remarks>
+    /// Advise you use System.Lazy by default, but if it doesn't play well in your struct environment,
+    /// this one should be useful
+    /// TODO: Consider also a thread safety arrangement like System.Lazy has
+    /// </remarks>
     public struct LazyLoader<T> where T : class, new()
     {
-        T value;
+        /// <summary>
+        /// Internal raw value, just for possible performance boost in specialized
+        /// scenarios where we know it's safe
+        /// </summary>
+        internal T value;
 
         public bool IsAllocated => value != default(T);
 
@@ -99,6 +109,67 @@ namespace Fact.Extensions.Memory
             if (lazyLoader.IsAllocated)
             {
                 return ifThen(lazyLoader.RawValue);
+            }
+            else
+            {
+                ifElse();
+                return default(TRetVal);
+            }
+        }
+    }
+
+
+    public static class LazyExtensions
+    {
+        public static TRetVal IfAllocated<T, TRetVal>(this Lazy<T> lazy, Func<T, TRetVal> ifThen, Func<TRetVal> ifElse)
+            where T : class, new()
+        {
+            if (lazy.IsValueCreated)
+            {
+                return ifThen(lazy.Value);
+            }
+            else
+            {
+                return ifElse();
+            }
+        }
+
+
+        /// <summary>
+        /// Returns value acquired from ifThen delegate if allocated, otherwise returns the default of TRetVal
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TRetVal"></typeparam>
+        /// <param name="lazy"></param>
+        /// <param name="ifThen"></param>
+        /// <returns></returns>
+        public static TRetVal IfAllocated<T, TRetVal>(this Lazy<T> lazy, Func<T, TRetVal> ifThen)
+            where T : class, new()
+        {
+            return lazy.IfAllocated(ifThen, () => default(TRetVal));
+        }
+
+
+        /// <summary>
+        /// Performs ifThen delegate if allocated, otherwise performs no action
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="lazy"></param>
+        /// <param name="ifThen"></param>
+        /// <returns></returns>
+        public static void IfAllocated<T>(this Lazy<T> lazy, Action<T> ifThen)
+            where T : class, new()
+        {
+            if (lazy.IsValueCreated) ifThen(lazy.Value);
+        }
+
+
+        public static TRetVal IfAllocated<T, TRetVal>(this Lazy<T> lazy, Func<T, TRetVal> ifThen, Action ifElse)
+            where T : class, new()
+        {
+            if (lazy.IsValueCreated)
+            {
+                return ifThen(lazy.Value);
             }
             else
             {
