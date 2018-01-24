a15084a1076929823ef5bfc8e4665a6c086563ab
Malachi Burke <malachi.burke@gmail.com>
2018-01-24 10:03:16 -0800

    Refinement of version/tagging scripts

--- src/setenv.sh
+++ src/setenv.sh
@@ -11,34 +11,7 @@ branch_name=$(${MB_USEFUL_SCRIPTS}/scm/get-branch-name.sh)
 # We must depend on setenv_windows or setenv_unix to set this
 #export NUGET='dotnet nuget'
 
-branch_version=$(<../.version.$branch_name)
-
-project_version=$(<../.project-version)
-
-if [ -z "$branch_version" ]
-then
-	branch_version=$(<../.version/$branch_name)
-
-	if [ -z "$branch_version" ]
-	then
-		echo "No version available for this branch.  Do not upload NUGET packages"
-		branch_version="0"
-	fi
-fi
-
-if [ -z $project_version ]
-then
-	echo "No version available for this project.  Do not upload NUGET packages"
-	project_version="0.0.1"
-fi
-
-printf -v branch_version "%03d" $branch_version
-
-export VERSION_PREFIX=$project_version
-export VERSION_SUFFIX=$branch_name$branch_version
-export VERSION=$VERSION_PREFIX-$VERSION_SUFFIX
-
-echo "Using VERSION=$VERSION"
+. ./setenv-version.sh
 
 #export branch_name
 #export branch_version
