61b91ab766c4b2b3d394270d1b2a7f9265222e33
Malachi Burke <malachi.burke@gmail.com>
2018-01-26 08:56:57 -0800

    Fixing namespaces

--- src/Fact.Extensions.Collection/ITaxonomyExtensions.cs
+++ src/Fact.Extensions.Collection/ITaxonomyExtensions.cs
@@ -1,50 +1,19 @@
-﻿using Fact.Extensions.Collection;
-using System;
+﻿using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Fact.Extensions.Experimental
+namespace Fact.Extensions.Collection
 {
     /// <summary>
     /// 
     /// </summary>
     public static class ITaxonomyExtensions
     {
-        public static void Visit<TNode, TContext>(this TNode node, Action<TNode, TContext> visitor, TContext context, int level = 0)
-            where TNode : IChildProvider<TNode>
-            where TContext : new()
-        {
-            visitor(node, context);
-
-            foreach (TNode childNode in node.Children)
-            {
-                context = new TContext();
-
-                Visit(childNode, visitor, context, level + 1);
-            }
-        }
-
-
-        public static void Visit<TNode>(this TNode node, Action<TNode> visitor, int level = 0)
-            where TNode : IChildProvider<TNode>
-        {
-            visitor(node);
-
-            foreach (TNode childNode in node.Children)
-            {
-                Visit(childNode, visitor, level + 1);
-            }
-        }
-
-
         public static void Visit<TNode>(this ITaxonomy<TNode> taxonomy, Action<TNode> visitor)
             where TNode : IChildProvider<TNode>, INamed
         {
-            Visit(taxonomy.RootNode, visitor);
+            taxonomy.RootNode.Visit(visitor);
         }
     }
-
-
-
 }
