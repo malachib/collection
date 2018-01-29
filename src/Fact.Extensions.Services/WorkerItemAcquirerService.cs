fe976ca367461544e645b5cd6f27edb48afc1096
Malachi Burke <malachi.burke@gmail.com>
2018-01-29 09:08:53 -0800

    Beefier logging/exception handling for WorkerItemAcquirerService

--- src/Fact.Extensions.Services/WorkerItemAcquirerService.cs
+++ src/Fact.Extensions.Services/WorkerItemAcquirerService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
@@ -10,9 +12,14 @@ namespace Fact.Extensions.Services
         WorkerServiceBase,
         IItemAcquirerService<T>
     {
+        readonly ILogger logger;
+
         public event Action<T> ItemAcquired;
 
-        public WorkerItemAcquirerService(ServiceContext context) : base(context) { }
+        public WorkerItemAcquirerService(ServiceContext context) : base(context)
+        {
+            logger = context.ServiceProvider.GetService<ILogger<WorkerItemAcquirerService<T>>>();
+        }
 
         protected abstract T GetItem(CancellationToken ct);
 
@@ -26,17 +33,32 @@ namespace Fact.Extensions.Services
                 {
                     return GetItem(context.CancellationToken);
                 }
-                catch(AggregateException e)
+                // FIX: Feel like we could make this a lot cleaner, but this
+                // should work at a minimum
+                catch (OperationCanceledException)
+                {
+                    cancelled = true;
+                    return default(T);
+                }
+                catch (AggregateException ae)
                 {
                     // FIX: Feel like we could make this a lot cleaner, but this
                     // should work at a minimum
-                    if (e.InnerException is OperationCanceledException)
+                    if (ae.InnerException is OperationCanceledException)
                     {
                         cancelled = true;
                         return default(T);
                     }
                     else
+                    {
+                        logger.LogWarning(0, ae, $"Unexpected agg-exception during Worker ({Name})");
                         throw;
+                    }
+                }
+                catch(Exception e)
+                {
+                    logger.LogWarning(0, e, $"Unexpected exception during Worker ({Name})");
+                    throw;
                 }
             });
 
