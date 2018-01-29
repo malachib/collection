f8ce53eb3629c77f4b66c43cf3892db056adda0f
Malachi Burke <malachi.burke@gmail.com>
2018-01-29 09:57:40 -0800

    Code organization

--- src/Fact.Extensions.Services/Extensions.cs
+++ src/Fact.Extensions.Services/Extensions.cs
@@ -117,46 +117,6 @@ namespace Fact.Extensions.Services
     }
 
 
-    public static class TaskExtensions
-    {
-        public static Task ContinueWithErrorLogger(this Task task, IServiceProvider sp, string name)
-        {
-            return ContinueWithErrorLogger(task, sp.GetService<ILoggerFactory>().CreateLogger(name), name);
-        }
-
-        /// <summary>
-        /// Wire in a generic post-task error logger
-        /// </summary>
-        /// <param name="task"></param>
-        /// <param name="logger"></param>
-        /// <param name="name"></param>
-        public static Task ContinueWithErrorLogger(this Task task, ILogger logger, string name = null)
-        {
-            return task.ContinueWith(t =>
-            {
-                if (t.IsFaulted)
-                {
-                    if (name == null) name = t.Id.ToString();
-
-                    if (t.Exception.InnerException is OperationCanceledException)
-                    {
-                        logger.LogWarning($"Task {name} shut down via operation canceled exception");
-
-                    }
-                    else
-                        logger.LogError(0, t.Exception.InnerException,
-                            $"Task {name} did not complete as expected");
-                }
-
-                if (t.IsCanceled)
-                {
-                    logger.LogDebug($"Task {name} completed unexceptionally");
-                }
-            });
-        }
-    }
-
-
     public static class ServiceManagerExtensions
     {
         public static IServiceDescriptor AddService(this 
