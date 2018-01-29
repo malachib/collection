81c60009d78024d3f7c3c2317689e96b215da3df
Malachi Burke <malachi.burke@gmail.com>
2018-01-29 15:23:19 -0800

    Switching alpha destination
    Prepping for more script consolidation

--- src/setenv.sh
+++ src/setenv.sh
@@ -2,16 +2,9 @@
 
 # TODO: move this out into "useful-scripts" project
 
-. ../ext/myget/setenv_nuget.sh
-. ../ext/useful-scripts/setenv.sh
-
-# Utilize .NET Core nuget, which is v4 as of this writing
-# We must depend on setenv_windows or setenv_unix to set this
-#export NUGET='dotnet nuget'
-
-export FACT_EXTENSIONS_COLLECTION_VERSIONPATH=$PWD/..
-
 . ./setenv-version.sh
 
-#export branch_name
-#export branch_version
+. ../ext/myget/setenv-nuget-alpha.sh
+. ../ext/useful-scripts/setenv.sh
+
+export FACT_EXTENSIONS_COLLECTION_VERSIONPATH=$PWD/..
