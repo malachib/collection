721238f159f68eb653ec736756ec2910497320a5
Malachi Burke <malachi.burke@gmail.com>
2018-02-07 08:52:50 -0800

    Commenting

--- src/Fact.Extensions.Services/ITransport.cs
+++ src/Fact.Extensions.Services/ITransport.cs
@@ -6,21 +6,44 @@ using System.Threading.Tasks;
 
 namespace Fact.Extensions.Services
 {
+    /// <summary>
+    /// Signature for event-firing notification of transport
+    /// sourced item acquisition
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IItemAcquirer<T>
     {
+        /// <summary>
+        /// Fired when item of interest has appeared on the transport
+        /// </summary>
         event Action<T> ItemAcquired;
     }
 
-
+    /// <summary>
+    /// Raw signature interface for async sending capability
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface ISenderAsync<T>
     {
-        Task<bool> SendAsync(T packet, CancellationToken cancellationToken);
+        /// <summary>
+        /// Send an item over the transport, asynchronously
+        /// </summary>
+        /// <param name="output"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>true if send was successful</returns>
+        Task<bool> SendAsync(T output, CancellationToken cancellationToken);
     }
 
 
     public interface ISender<T>
     {
-        bool Send(T packet, CancellationToken cancellationToken);
+        /// <summary>
+        /// Send an item over the transport
+        /// </summary>
+        /// <param name="output"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>true if send was successful</returns>
+        bool Send(T output, CancellationToken cancellationToken);
     }
 
 
