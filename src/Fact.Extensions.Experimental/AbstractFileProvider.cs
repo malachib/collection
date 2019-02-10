c78bbbd2ed01d7e4c76b1526aca29511d0f4a6e7
Malachi Burke <malachi.burke@gmail.com>
2019-02-10 07:54:22 -0800

    Starting to build out abstracted file provider

---
+++ src/Fact.Extensions.Experimental/AbstractFileProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Fact.Extensions.Collection;
+
+namespace Fact.Extensions.Experimental
+{
+    public class AbstractFileProvider<TAccessor> where TAccessor: INamedAccessor<object>
+    {
+        TAccessor files;
+
+        public AbstractFileProvider()
+        {
+        }
+    }
+}
