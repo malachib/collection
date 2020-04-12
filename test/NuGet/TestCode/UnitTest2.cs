717a04a12a141696d6582dd9e92ebffd9ee8cd1f
Malachi Burke <malachi.burke@gmail.com>
2020-04-12 10:17:06 -0700

    Adding shared test code (mostly) boilerplate

---
+++ test/NuGet/TestCode/UnitTest2.cs
@@ -0,0 +1,35 @@
+﻿#if TEST_DIAGNOSTIC
+#define TEST_ENABLED
+#endif
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TestCode
+{
+#if TEST_ENABLED
+    using Fact.Extensions.Collection;
+#endif
+
+    public class UnitTest2
+    {
+#if TEST_ENABLED
+        public class Node : INamed
+        {
+            public string Name { get; set; }
+        }
+#endif
+
+        [Fact]
+        public void Create_NamedChildCollection()
+        {
+#if TEST_ENABLED
+            var childCollection = new NamedChildCollection<Node>("root");
+
+            childCollection.AddChild(new Node() { Name = "Node 1" });
+#endif
+        }
+    }
+}
