8024478c8a1dcbf3e8272ebebcaeb1d22db41078
Malachi Burke <malachi.burke@gmail.com>
2021-08-17 19:21:35 -0700

    Adding xUnit for Fact.Extensions.Services
    Since I like xUnit a lot more than MSTest

---
+++ src/Fact.Extensions.Services.xUnit/ServiceLifecycleTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Fact.Extensions.Services.xUnit
+{
+    public class ServiceLifecycleTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
